6634a2e06010067303cecb53ff11a7226fc5a12a
Sebastien Duquette <sduquette@devolutions.net>
2018-12-07 11:15:20 -0500

    reduce the case further

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,13 @@ use std::time::Duration;
 
 use nng::{Aio, Context, Message, Protocol, Socket};
 
-const URL_CLIENT: &str = "ipc://nng_client";
-const URL_SERVER: &str =  "ipc://nng_server";
+const URL_SERVER: &str = "ipc://nng_server";
 
 fn client() -> Result<(), nng::Error> {
-    let url_rep = URL_CLIENT;
     let url_req = URL_SERVER;
-
-    let mut rep = Socket::new(Protocol::Rep0)?;
-    rep.listen(url_rep)?;
 
     let mut req = Socket::new(Protocol::Req0)?;
     req.dial(url_req)?;
-    let _ = req.send(Message::new()?);
-    println!("message sent");
-
-    println!("waiting for connect back");
-    let _msg = rep.recv()?;
 
     loop {
         let _ = req.send(Message::new()?);
@@ -30,18 +20,10 @@ fn client() -> Result<(), nng::Error> {
 
 fn server() -> Result<(), nng::Error> {
     let url_rep = URL_SERVER;
-    let url_req = URL_CLIENT;
 
     let mut rep = Socket::new(Protocol::Rep0)?;
     rep.listen(url_rep)?;
-    let msg = rep.recv()?;
 
-    println!("connection on {}, connecting back to client", url_rep);
-    
-    let mut req = Socket::new(Protocol::Req0)?;
-    req.dial(url_req)?;
-    let _ = req.send(Message::new()?);
-    println!("message sent");
     let context = Context::new(&rep)?;
 
     let aio = Aio::with_callback(move |aio: &Aio| {
@@ -50,9 +32,11 @@ fn server() -> Result<(), nng::Error> {
         }
     })?;
 
+    // thread::sleep(Duration::from_millis(100));
+
     loop {
         context.recv(&aio)?;
-        //thread::sleep(Duration::from_millis(100));
+        // thread::sleep(Duration::from_millis(100));
     }
 }
 
